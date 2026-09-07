a2760a3b8e9fed821af7d6a5870ada0cfb414c6b
Jonas-Krebber <jonaskrebber074@gmail.com>
2026-09-07 22:01:03 +0200

    Debugging

--- Matrix.py
+++ Matrix.py
@@ -2,8 +2,14 @@ class Blockmatrix():
 
     def __init__(self, values):
 
-        # Checks for right input - missing: Checking each nested list element
-        assert isinstance(values, list), "value must be a number."
+        # Checks for right input
+        assert isinstance(values, list), "Entry must be an array"
+
+        for i in range(len(values)):
+            # Missing: Check if all arrays are of the same length
+
+            for j in range(len(values[i])):
+                assert type(values[i][j]) in [int, float], "Entry must consist of only numbers"
 
         # Defining the Matrix parameters 
         self.values = values
@@ -12,14 +18,19 @@ class Blockmatrix():
     def __str__(self):
         str = ''
         for i in range(len(self.values)):
-            for j in range(len(self.values[0])):
+            for j in range(len(self.values[i])):
                str += f'{self.values[i][j]} '
             str += '\n'
         return str
 
     def __add__(self, other):
 
-        # Missing - Check if the computation is possible:
+        # Checks if the computation is possible
+
+        assert len(self.values) == len(other.values), "Computation not possible, check row dimension"
+
+        for i in range(len(self.values)):
+            assert len(self.values[i]) == len(other.values[i]), "Computation not possible, check column dimension"
 
         # Add the two matrices 
 
@@ -33,7 +44,7 @@ class Blockmatrix():
         return Blockmatrix(newValues)
             
 
-matrix1 = Blockmatrix([[2,3,3], [1,1,0]])
+matrix1 = Blockmatrix([[2,3, 0], [1,1,0]])
 matrix2 = Blockmatrix([[1,1,1], [0,0,0]])
 
 print(matrix1 + matrix2)
