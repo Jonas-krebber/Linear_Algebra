e038989a1ea0bc02574dfe08599f846f8d3706ac
Jonas-Krebber <jonaskrebber074@gmail.com>
2026-09-08 20:27:45 +0200

    Transpose function, Dot product

--- Matrix.py
+++ Matrix.py
@@ -1,26 +1,42 @@
+# Create a class called Matrix. It performs all basic functions of linear algebra in combination with any matrix.
+# Current operations: +, -, *, =
+# Current functions: transpose
+
+
 class Matrix():
 
     def __init__(self, matrix):
 
         # Checks for right input
+
         assert isinstance(matrix, list), "Entry must be an array"
 
         for i in range(len(matrix)):
+
             assert len(matrix[i]) == len(matrix[0]), "Matrix misses values"
 
             for j in range(len(matrix[i])):
+
                 assert type(matrix[i][j]) in [int, float], "Entry must consist of only numbers"
 
         # Defining the Matrix parameters 
+
         self.matrix = matrix
 
-    # Defining the interface for displaying the Matrix     
+    # Defining the interface for displaying the Matrix   
+  
     def __str__(self):
+
         str = ''
+
         for i in range(len(self.matrix)):
+
             for j in range(len(self.matrix[i])):
+               
                str += f'{self.matrix[i][j]} '
+
             str += '\n'
+
         return str
 
     def __add__(self, other):
@@ -36,8 +52,11 @@ class Matrix():
         newMatrix = []
 
         for i in range(len(self.matrix)):
+
             newMatrix.append([])
+
             for j in range(len(self.matrix[0])):
+
                 newMatrix[i].append(self.matrix[i][j] + other.matrix[i][j])
 
         return Matrix(newMatrix)
@@ -55,8 +74,11 @@ class Matrix():
             newMatrix = []
     
             for i in range(len(self.matrix)):
+
                 newMatrix.append([])
+
                 for j in range(len(self.matrix[0])):
+
                     newMatrix[i].append(self.matrix[i][j] - other.matrix[i][j])
     
             return Matrix(newMatrix)
@@ -68,16 +90,39 @@ class Matrix():
         assert len(self.matrix) == len(other.matrix), "Computation not possible, check row dimension"
             
         for i in range(len(self.matrix)):
+
             assert len(self.matrix[i]) == len(other.matrix[i]), "Computation not possible, check column dimension"
 
         # Compares if the two matrices are equal to each other
 
         for i in range(len(self.matrix)):
+
             for j in range(len(self.matrix[i])):
+
                 if self.matrix[i][j] != other.matrix[i][j]:
+
                     return False
                 
         return True 
+
+    def transpose(self):
+
+        # Transposes the Matrix
+
+        newMatrix = []
+
+        for i in range(len(self.matrix[0])):
+
+            newRow = []
+
+            for j in range(len(self.matrix)):
+
+                newRow.append(self.matrix[j][i])
+
+            newMatrix.append(newRow)
+
+
+        return Matrix(newMatrix)
 
     def __mul__(self, other):
 
@@ -89,16 +134,27 @@ class Matrix():
 
         newMatrix = []
 
+        other = other.transpose()
+
         # Missing - transpose function to calculate the dot product 
-        for i in range(len(self.matrix)):
+        
+        for i in range(len(other.matrix)):
+
             newMatrix.append([])
 
-            for j in range(len(other.matrix[0])):
-                newMatrix[i].append(0)
+            for j in range(len(self.matrix)):
+                
+                sum = 0
 
-        return Matrix(newMatrix)
+                for k in range(len(self.matrix[0])):
 
-matrix1 = Matrix([[0,0,0], [0,0,0]])
-matrix2 = Matrix([[0,0], [0,0], [0,0]])
+                    sum += self.matrix[j][k] * other.matrix[i][k]
+
+                newMatrix[i].append(sum)
+                
+        return Matrix(newMatrix).transpose()
+
+matrix1 = Matrix([[1,2], [3,1]])
+matrix2 = Matrix([[2,0,3], [1,1,0]])
 
 print(matrix1 * matrix2)
