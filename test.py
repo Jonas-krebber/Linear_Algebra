34f4a986514a1eea3050953278a375b0d88ceae4
Jonas-Krebber <jonaskrebber074@gmail.com>
2026-09-20 18:13:23 +0200

    Adding matplotlib graphing of computational speeds

--- test.py
+++ test.py
@@ -1,60 +1,87 @@
 import Matrix as M
 import numpy as np
 import timeit as t
+import matplotlib.pyplot as plt
 
-matrix1 = M.Matrix([[1,2,-1], [2,1,2], [-1,2,1]])
-matrix2 = M.Matrix([[1,1,0], [1,1,0], [1,1,1]])
+# matrix1 = M.Matrix([[1,2,-1], [2,1,2], [-1,2,1]])
+# matrix2 = M.Matrix([[1,1,0], [1,1,0], [1,1,1]])
 
-matrix3 = M.Matrix([[1,2],[0,0],[2,2],[1,4]])
-matrix4 = M.Matrix([[0,1,1,5], [-1,-2,-3,0]])
-matrix5 = M.Matrix([[8,0,5,11,5], [16,13,18,9,16], [0,1,1,15,19], [2,1,12,17,10], [14,7,12,0,12]])
+# matrix3 = M.Matrix([[1,2],[0,0],[2,2],[1,4]])
+# matrix4 = M.Matrix([[0,1,1,5], [-1,-2,-3,0]])
+# matrix5 = M.Matrix([[8,0,5,11,5], [16,13,18,9,16], [0,1,1,15,19], [2,1,12,17,10], [14,7,12,0,12]])
 
-print(matrix1.trace())
-print(matrix1.pow(3))
-print(matrix1.transpose())
-print(matrix1.inverse())
+# print(matrix1.trace())
+# print(matrix1.pow(3))
+# print(matrix1.transpose())
+# print(matrix1.inverse())
 
-print(matrix1 == matrix2 + matrix1 - matrix2)
-print(matrix1 + matrix2)
-print(matrix1 - matrix2)
-print(matrix3 * matrix4)
+# print(matrix1 == matrix2 + matrix1 - matrix2)
+# print(matrix1 + matrix2)
+# print(matrix1 - matrix2)
+# print(matrix3 * matrix4)
 
-npmatrix1 = np.array([[1,2,-1], [2,1,2], [-1,2,1]])
-npmatrix2 = np.array([[8,0,5,11,5], [16,13,18,9,16], [0,1,1,15,19], [2,1,12,17,10], [14,7,12,0,12]])
-
-# Time for np.invert
-npt1 = t.default_timer()
-
-np.invert(npmatrix2)
-
-npt2 = t.default_timer()
-
-difnp = npt2-npt1
-
-print(difnp, 'Time for np.invert()')
-
-# Time for np.linal.inv()
-nplint1 = t.default_timer()
-
-np.linalg.inv(npmatrix2)
-
-nplint2 = t.default_timer()
-
-difnplin = nplint2-nplint1
-
-print(difnplin, 'Time for np.linalg.inv()')
+# npmatrix1 = np.array([[1,2,-1], [2,1,2], [-1,2,1]])
+# npmatrix2 = np.array([[8,0,5,11,5], [16,13,18,9,16], [0,1,1,15,19], [2,1,12,17,10], [14,7,12,0,12]])
 
 
-# Time for M.inverse()
-t1 = t.default_timer()
+# Generate matrices of a given dimension (n) that are invertable 
+def randomMatrix(n):
+    return np.vander(np.arange(n), n)
 
-matrix5.inverse()
+timeNPInvert = []
+timeNPLinalgInv = []
+timeInverse = []
 
-t2 = t.default_timer()
+n = 1000
 
-difM = t2-t1
+# Max dimension for np.linalg.inv is 124
 
-print(difM, 'Time for M.inverse()')
+for i in range(n):
+    # Time for np.invert
+    t1NPInvert = t.default_timer()
 
-# print(difM/difnp)
-# print(difnp/difM)
+    np.invert(randomMatrix(i))
+
+    t2NPInvert = t.default_timer()
+
+    timeNPInvert.append(t2NPInvert-t1NPInvert)
+
+    # Time for np.linalg.inv
+    t1NPLinalgInv = t.default_timer()
+    
+    np.linalg.inv(randomMatrix(i))
+
+    t2NPLinalgInv = t.default_timer()
+
+    timeNPLinalgInv.append(t2NPLinalgInv-t1NPLinalgInv)
+
+    # Time for self programmed function 
+    t1Inverse = t.default_timer()
+        
+    M.Matrix(randomMatrix(10).tolist()).inverse()
+
+    t2Inverse = t.default_timer()
+
+    timeInverse.append(t2Inverse-t1Inverse)
+
+
+#print(timeNPInvert)
+#print(timeNPLinalgInv)  
+#print(timeInverse)
+
+dataX = np.arange(n)
+
+
+plt.figure(1)
+
+plt.title('Time for calculating the inverse of a vander matrix with dimension n')
+
+plt.plot(dataX, timeNPInvert, 'b*', label='Numpy Invert function')
+# plt.plot(dataX, timeNPLinalgInv, 'r*', label='Numpy linalg function')
+plt.plot(dataX, timeInverse, 'g*', label='Python Invert function')
+
+plt.xlabel('Dimension (n)')
+plt.ylabel('Computing time')
+plt.legend()
+
+plt.show()
